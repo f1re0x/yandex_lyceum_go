9e59e36514aa1629a5ce2fd2e20cedeaf4632300
Евгений Шелухин <F1re0x1@yandex.ru>
2024-10-01 18:08:52 +0400

    Initial commit

--- prstfile.py
+++
@@ -1,6 +0,0 @@
-try:
-    with open('text.txt', 'r', encoding='utf-8') as file:
-        print(file.read())
-except FileNotFoundError:
-    print("Файл не найден")
-
--- tgbot/main.py
+++
@@ -1,6 +0,0 @@
-kol = 0
-for i in range(1, 627 + 1):
-    if i % 19 == 0:
-        kol += 1
-print(kol)
-        
