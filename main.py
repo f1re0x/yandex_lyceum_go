f823014225da965cfcde672a341f69f9053fa4a2
Евгений Шелухин <F1re0x1@yandex.ru>
2024-10-06 11:09:57 +0400

    prst commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-print("x y z w")
-for x in range(0, 2):
-    for y in range(0, 2):
-        for z in range(0, 2):
-            for w in range(0, 2):
-                if (x and y and not(z)) == (y or z or not(w)):
-                    print(x, y, z, w)
+from itertools import *
+def f(x,y,z,w):
+    return (not(x <= w)) or (y == z) or y
+for a1,a2,a3,a4,a5,a6,a7 in product([0,1], repeat=7): # Перебираем значения
+    table = [(a1,1,a2,0),(a3,0,1,a4),(a5,a6,0,a7)]
+    if len(table) == len(set(table)): #Проверяем уникальность таблицы
+        for p in permutations('xyzw'):
+            if [f(**dict(zip(p,r))) for r in table] == [0,0,0]: #Подставляем соответствующие значения xyzw
+                print(*p, sep='')
